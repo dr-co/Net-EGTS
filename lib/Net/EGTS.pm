eeaf13565e76f6b70c628fd2217eae4263fd8992
Roman V. Nikolaev <rshadow@rambler.ru>
2018-03-16 16:49:52 +0300

    template for main file

---
+++ lib/Net/EGTS.pm
@@ -0,0 +1,15 @@
+use utf8;
+
+package Net::EGTS;
+use namespace::autoclean;
+use Mouse;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+Net::EGTS - Perl Interface to EGTS protocol. GOST R 56360-2015.
+
+=cut
+
+__PACKAGE__->meta->make_immutable();
