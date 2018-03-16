51a03fa34097e475ac9f6cb89f269368dc11a9a6
Roman V. Nikolaev <rshadow@rambler.ru>
2018-03-16 15:07:56 +0300

    template fro example script

---
+++ script/example.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use utf8;
+use open qw(:std :utf8);
+use lib qw(lib);
+
+use Net::EGTS::Util;
+use Net::EGTS::Simple;
+
+my $client = Net::EGTS::Simple->new(
+        host    => 'rnis.mos.ru',
+        port    => 4045,
+        did     => 0,
+);
+$client->connect or die 'Can`t connect';
+
+my $result1 = $client->auth;
+die $result1 unless ref $result1;
+
+my $result2 = $client->teledata({
+    id          => 1,
+    time        => str2time( time ),
+    longitude   => 33,
+    latitude    => 55,
+    avg_speed   => 20,
+    direction   => 180,
+    order       => 0,
+});
+die $result2 unless ref $result2;
+
+$client->disconnect or die 'Can`t disconnect';
