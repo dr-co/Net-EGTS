9adc85bea26d6a80e58639e1553188ec8e1e2459
Roman V. Nikolaev <rshadow@rambler.ru>
2018-03-18 00:08:15 +0300

    more types and simple defines

--- lib/Net/EGTS/Types.pm
+++ lib/Net/EGTS/Types.pm
@@ -6,19 +6,26 @@ use Mouse;
 use Mouse::Util::TypeConstraints;
 
 subtype 'BOOLEAN',  as 'Bool';
-subtype 'BYTE',     as 'Int',   where { 0 <= $_ && $_ <= 255 };
-subtype 'USHORT',   as 'Int',   where { 0 <= $_ && $_ <= 65535 };
-subtype 'UINT',     as 'Int',   where { 0 <= $_ && $_ <= 4294967295 };
-subtype 'ULONG',    as 'Int',   where { 0 <= $_ && $_ <= 18446744073709551615 };
-subtype 'SHORT',    as 'Int',   where { -32768 <= $_ && $_ <= 32767 };
-subtype 'INT',      as 'Int',   where { -2147483648 <= $_ && $_ <= 2147483647 };
+subtype 'BYTE',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 8 };
+subtype 'BYTE3',    as 'Int',   where { 0 <= $_ && $_ < 2 ** 24 };
+subtype 'USHORT',   as 'Int',   where { 0 <= $_ && $_ < 2 ** 16 };
+subtype 'UINT',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 32 };
+subtype 'ULONG',    as 'Int',   where { 0 <= $_ && $_ < 2 ** 64 };
+subtype 'SHORT',    as 'Int',   where { -(2 ** 15) <= $_ && $_ < 2 ** 15 };
+subtype 'INT',      as 'Int',   where { -(2 ** 31) <= $_ && $_ < 2 ** 31 };
 subtype 'FLOAT',    as 'Num';
 subtype 'DOUBLE',   as 'Num';
 subtype 'STRING',   as 'Str';
 subtype 'BINARY',   as 'Str';
 
 subtype 'BIT1',     as 'Bool';
-subtype 'BIT2',     as 'Int', where { 0 <= $_ && $_ <= 3 };
+subtype 'BIT2',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 2 };
+subtype 'BIT3',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 3 };
+subtype 'BIT4',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 4 };
+subtype 'BIT5',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 5 };
+subtype 'BIT6',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 6 };
+subtype 'BIT7',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 7 };
+subtype 'BIT8',     as 'Int',   where { 0 <= $_ && $_ < 2 ** 8 };
 
 subtype 'uInt',     as 'Int', where { 0 <= $_ };
 
